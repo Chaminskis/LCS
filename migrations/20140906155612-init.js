b448a06c66a111983dd0803fd5082630d5bcacd5
Blashadow-Linux <blackzerogamer@gmail.com>
2014-09-14 11:24:57 -0400

    migrations updated

--- migrations/20140906155612-init.js
+++ migrations/20140906155612-init.js
@@ -2,11 +2,18 @@ module.exports = {
   up: function(migration, DataTypes, done) {
     
   	migration.createTable('hospitals',{
-  		id:{ type: DataTypes.INTEGER, autoIncrement: true, unique:true },
-		  title:{ type: DataTypes.STRING },
-      updated_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-      created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-      deleted_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+  		
+        id:{ type: DataTypes.INTEGER, autoIncrement: true, unique:true },
+		title:{ type: DataTypes.STRING },
+        details:{ type: DataTypes.STRING },
+        address:{ type: DataTypes.STRING },
+
+        latitude:{ type: DataTypes.DECIMAL },
+        longuitude:{ type: DataTypes.DECIMAL },
+
+        updated_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+        created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+        deleted_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
   	},{
   		engine:'INNODB',
         timestamps: true,

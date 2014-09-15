4d9ef7e2928f8eb73f76b038b58e6635267707f0
carbonelldx <carbonelldx89@gmail.com>
2014-09-15 15:50:06 -0400

    dangerous marge to config-debug....jojojojoj...

--- public/js/admin/controller/home_controller.js
+++ public/js/admin/controller/home_controller.js
@@ -7,6 +7,16 @@
 
 angular.module('app.controllers')
 
-.controller('home',['$scope',function($scope){
+.controller('home', 'hospital', ['$scope',function($scope, home, hospital){
 	$scope.message = 'nice from controller';
+
+$scope.load = function(){
+	hospital.list().then(function(data){
+		alert("I'm loaded!");
+		$scope.data = data.result;
+	},function(error){
+		window.alert('Error ' + error);
+	});
+};
+
 }]);

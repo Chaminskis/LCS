0eb1d67086a9aa81df59f939b4120718affd0e0b
carbonelldx <carbonelldx89@gmail.com>
2014-09-15 21:33:30 -0400

    module declaration bug fix

--- public/js/admin/controller/home_controller.js
+++ public/js/admin/controller/home_controller.js
@@ -5,18 +5,18 @@
 
 'use strict';
 
-angular.module('app.controllers')
+angular.module('app.controllers', [])
 
-.controller('home', 'hospital', ['$scope',function($scope, home, hospital){
+.controller('home', ['$scope',  function($scope, home, hospital){
 	$scope.message = 'nice from controller';
 
 $scope.load = function(){
-	hospital.list().then(function(data){
-		alert("I'm loaded!");
-		$scope.data = data.result;
-	},function(error){
-		window.alert('Error ' + error);
-	});
+	// hospital.list().then(function(data){
+	// 	alert("I'm loaded!");
+	// 	$scope.data = data.result;
+	// },function(error){
+	// 	window.alert('Error ' + error);
+	// });
 };
 
 }]);

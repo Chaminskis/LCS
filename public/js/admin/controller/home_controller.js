632db63107b011733d9e73aff3f5e37b3f396630
carbonelldx <carbonelldx89@gmail.com>
2014-09-16 06:35:59 -0400

    adding angular-route.min.js.map and some nasty errors to my code

--- public/js/admin/controller/home_controller.js
+++ public/js/admin/controller/home_controller.js
@@ -7,13 +7,14 @@
 
 angular.module('app.controllers', [])
 
-.controller('home', ['$scope',  function($scope, home, hospital){
+.controller('home', ['$scope', 'Hospital',  function($scope, home, hospital){
 	$scope.message = 'nice from controller';
-
+alert("you, loading??");
 $scope.load = function(){
-	hospital.list().then(function(data){
+	Hospital.list().then(function(data){
 		alert("I'm loaded!");
 		$scope.data = data.result;
+		console.log($scope.data);
 	},function(error){
 		window.alert('Error ' + error);
 	});

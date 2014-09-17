8d9a6a343162d64ffad3258f8cc56c00b90206fc
BlackShadow <blackzerogamer@gmail.com>
2014-09-17 09:27:22 -0400

    sidebar

--- public/js/admin/controller/home_controller.js
+++ public/js/admin/controller/home_controller.js
@@ -9,4 +9,16 @@ angular.module('app.controllers')
 
 .controller('HomeCtrl',['$scope',function($scope){
 	$scope.message = 'nice from controller';
+	
+	$scope.sectionActive = 'home';
+	
+	$scope.setActive = function(section){
+	    $scope.sectionActive = section;
+	};
+	
+	$scope.active = function(section){
+	    
+	    return section == $scope.sectionActive;
+	};
+	
 }]);

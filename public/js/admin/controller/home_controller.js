f87567e050ef8466c29f9074741fad6c9d8946c8
carbonelldx <carbonelldx89@gmail.com>
2014-09-19 04:17:25 -0400

    retrieving hospitals from db

--- public/js/admin/controller/home_controller.js
+++ public/js/admin/controller/home_controller.js
@@ -8,16 +8,22 @@
 angular.module('app.controllers', ['app.services'])
 
 .controller('home', ['$scope', 'Hospital',  function($scope, home, hospital){
-	$scope.message = 'nice from controller';
-alert("you, loading??");
-$scope.load = function(){
-	Hospital.list().then(function(data){
-		alert("I'm loaded!");
-		$scope.data = data.result;
-		console.log($scope.data);
-	},function(error){
-		window.alert('Error ' + error);
-	});
-};
+	// $scope.message = 'nice from controller';
 
+	// $scope.$on('$viewContentLoaded', function(){
+	    //Here your view content is fully loaded !!
+	// });
+
+	var setup = function(){
+		// alert("loaded");
+		Hospital.list().then(function(data){
+				// alert("I'm loaded!");
+				$scope.data = data.result;
+				console.log($scope.data);
+			},function(error){
+				window.alert('Error ' + error);
+		});  		
+	}
+
+	setup();
 }]);

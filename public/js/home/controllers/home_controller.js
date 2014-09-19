865fc213633eb1769492f695997fc8eb6021d509
carbonelldx <carbonelldx89@gmail.com>
2014-09-19 04:51:59 -0400

    removing duplicated controller

--- public/js/home/controllers/home_controller.js
+++ public/js/home/controllers/home_controller.js
@@ -6,7 +6,35 @@
 'use strict';
 
 angular.module('app.controllers')
+.controller('home', ['$scope', 'Hospital',  function($scope, home, hospital){
+	// $scope.message = 'nice from controller';
 
-.controller('home',['$scope',function($scope){
-	$scope.message = 'nice from controller';
+	// $scope.$on('$viewContentLoaded', function(){
+	    //Here your view content is fully loaded !!
+	// });
+
+	// $scope.sectionActive = 'home';
+	
+	// $scope.setActive = function(section){
+	//     $scope.sectionActive = section;
+	// };
+	
+	// $scope.active = function(section){
+	    
+	//     return section == $scope.sectionActive;
+	// };
+
+
+	var setup = function(){
+		// alert("loaded");
+		Hospital.list().then(function(data){
+				// alert("I'm loaded!");
+				$scope.data = data.result;
+				console.log($scope.data);
+			},function(error){
+				window.alert('Error ' + error);
+		});  		
+	}
+
+	setup();
 }]);

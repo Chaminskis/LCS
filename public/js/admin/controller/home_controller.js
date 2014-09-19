a84c144b4730828aae8373eae04bfc9f8794ea35
carbonelldx <carbonelldx89@gmail.com>
2014-09-19 04:24:39 -0400

    merging more files

--- public/js/admin/controller/home_controller.js
+++ public/js/admin/controller/home_controller.js
@@ -6,14 +6,24 @@
 'use strict';
 
 angular.module('app.controllers')
-
-<<<<<<< HEAD
 .controller('home', ['$scope', 'Hospital',  function($scope, home, hospital){
 	// $scope.message = 'nice from controller';
 
 	// $scope.$on('$viewContentLoaded', function(){
 	    //Here your view content is fully loaded !!
 	// });
+
+	// $scope.sectionActive = 'home';
+	
+	// $scope.setActive = function(section){
+	//     $scope.sectionActive = section;
+	// };
+	
+	// $scope.active = function(section){
+	    
+	//     return section == $scope.sectionActive;
+	// };
+
 
 	var setup = function(){
 		// alert("loaded");
@@ -27,20 +37,4 @@ angular.module('app.controllers')
 	}
 
 	setup();
-=======
-.controller('HomeCtrl',['$scope',function($scope){
-	$scope.message = 'nice from controller';
-	
-	$scope.sectionActive = 'home';
-	
-	$scope.setActive = function(section){
-	    $scope.sectionActive = section;
-	};
-	
-	$scope.active = function(section){
-	    
-	    return section == $scope.sectionActive;
-	};
-	
->>>>>>> 15313f6297378da4edae15550c9707ae8a4b034a
 }]);
